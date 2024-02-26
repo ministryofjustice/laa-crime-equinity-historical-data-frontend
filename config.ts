abe11aa27a7625d4935f174d2b327233a90e2409
Sarahyjja <https://github.com/sarahyjja>
2024-02-26 16:39:35 +0000

    🍪 EMP-76: Setting up for future prod env.

--- config.ts
+++ config.ts
@@ -1,5 +1,7 @@
+const production = process.env.NODE_ENV === 'production'
 export default {
   buildNumber: 1,
   gitRef: 1,
   staticResourceCacheDuration: '1h',
+  https: production
 }
--- middleware/setUpWebSession.ts
+++ middleware/setUpWebSession.ts
@@ -1,11 +1,12 @@
 import session from 'express-session'
 import express, { Router } from 'express'
+import config from "../config";
 
 export default function setUpWebSession(): Router {
   const router = express.Router()
   router.use(
     session({
-      cookie: { secure: true, sameSite: 'lax', maxAge: 60000 },
+      cookie: { secure: config.https, sameSite: 'lax', maxAge: 60000 },
       secret: 'secret',
       saveUninitialized: false,
       resave: false,
