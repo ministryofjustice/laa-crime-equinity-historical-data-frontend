c390d2b68bbda7e695128740bf1629c030cb9931
Sarahyjja <https://github.com/sarahyjja>
2024-03-06 15:32:28 +0000

    EMP-155: 🌱 Update setup file

--- routes/testutils/appSetup.ts
+++ routes/testutils/appSetup.ts
@@ -1,3 +1,5 @@
+
+/*
 import express, { Express } from 'express'
 import createError from 'http-errors'
 import bodyParser from 'body-parser'
@@ -19,6 +21,40 @@ function appSetup(): Express {
   app.use(bodyParser.urlencoded({ extended: true }))
   routes(app)
   app.use((req, res, next) => next(createError(404, 'Not found')))
+  app.use(errorHandler())
+
+  return app
+}
+
+export default function appWithAllRoutes(): Express {
+  return appSetup()
+}
+*/
+
+import express, { Express } from 'express'
+import { NotFound } from 'http-errors'
+
+import routes from '../index'
+import nunjucksSetup from '../../utils/nunjucksSetup'
+import errorHandler from '../../errorHandler'
+
+
+function appSetup(): Express {
+  const app = express()
+
+  app.set('view engine', 'njk')
+
+  nunjucksSetup(app)
+
+  app.use((req, res, next) => {
+    res.locals = {}
+    next()
+  })
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use('/', routes)
+  app.use((req, res, next) => next(new NotFound))
   app.use(errorHandler())
 
   return app
