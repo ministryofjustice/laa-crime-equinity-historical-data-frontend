29008f1888115ef5863da59d2586b4af33fe55d1
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-05-13 12:18:14 +0100

    EMP-258 Created navigation bar

--- server/controllers/crm5Controller.ts
+++ server/controllers/crm5Controller.ts
@@ -9,7 +9,62 @@ export default class Crm5Controller {
     return async (req: Request, res: Response): Promise<void> => {
       const usn = Number(req.params.usn)
       const crm5Response = await this.crm5Service.getCrm(usn)
-      res.render('pages/crmDetails', { title: 'CRM5', data: crm5Response })
+      const navLink = `/crm5/${usn}`
+      const navigationItems = {
+        label: 'Side navigation',
+        items: [
+          {
+            text: 'General Information',
+            href: `${navLink}/1`,
+            active: true,
+          },
+          {
+            text: 'Firm Details',
+            href: `${navLink}/2`,
+          },
+          {
+            text: "Client's Details",
+            href: '#',
+          },
+          {
+            text: 'Capital Details',
+            href: '#',
+          },
+          {
+            text: 'Income Details',
+            href: '#',
+          },
+          {
+            text: 'Advice and Assistance',
+            href: '#',
+          },
+          {
+            text: 'Solicitors Declaration',
+            href: '#',
+          },
+          {
+            text: 'Court of Appeal Funding',
+            href: '#',
+          },
+          {
+            text: 'Details of Work Completed',
+            href: '#',
+          },
+          {
+            text: 'Costs',
+            href: '#',
+          },
+          {
+            text: 'Case History',
+            href: '#',
+          },
+          {
+            text: "Solicitor's Certification",
+            href: '#',
+          },
+        ],
+      }
+      res.render('pages/crmDetails', { title: 'CRM5', data: crm5Response, navigationItems })
     }
   }
 }
