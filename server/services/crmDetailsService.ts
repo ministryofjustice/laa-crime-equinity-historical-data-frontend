9b55235391d2c62ae6f361afac8e033e368310e7
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-05-16 11:55:07 +0100

    Added schema validation for crm details config

--- server/services/crmDetailsService.ts
+++ server/services/crmDetailsService.ts
@@ -1,3 +1,4 @@
+import Joi from 'joi'
 import crm5DetailConfig from './config/crm5DetailsConfig.json'
 
 type Field = {
@@ -20,8 +21,30 @@ type CRMDetailConfig = {
   sections: Array<Section>
 }
 
+const schema = Joi.object({
+  sections: Joi.array().items({
+    urlPath: Joi.string().required(),
+    title: Joi.string().required(),
+    subsections: Joi.array().items({
+      title: Joi.string().required(),
+      fields: Joi.array().items({
+        label: Joi.string().required().allow(''),
+        apiField: Joi.string().required().allow(null),
+      }),
+    }),
+  }),
+})
+
+const getValidConfig = (config: CRMDetailConfig, configName: string) => {
+  const { error } = schema.validate(config)
+  if (error?.details) {
+    throw new Error(`Invalid ${configName} Details config: ${JSON.stringify(error.details)}`)
+  }
+  return config
+}
+
 export default class CrmDetailsService {
   getCrm5DetailConfig(): CRMDetailConfig {
-    return crm5DetailConfig
+    return getValidConfig(crm5DetailConfig, 'CRM5')
   }
 }
