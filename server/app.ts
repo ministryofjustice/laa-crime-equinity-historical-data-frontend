b9077d7ebefc3b7b6f6d5b9953ed0915cebcddc6
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-05-21 14:01:10 +0100

    Added auth routes

--- server/app.ts
+++ server/app.ts
@@ -15,6 +15,7 @@ import setUpWebSecurity from './middleware/setUpWebSecurity'
 import setUpWebSession from './middleware/setUpWebSession'
 
 import routes from './routes'
+import authRoutes from './routes/auth'
 
 import type { Services } from './services'
 import { controllers } from './controllers'
@@ -37,6 +38,7 @@ export default function createApp(services: Services): express.Application {
   app.use(setUpCsrf())
 
   app.use('/', routes(controllers(services)))
+  app.use('/auth', authRoutes())
 
   app.use((req, res, next) => next(createError(404, 'Not found')))
   app.use(errorHandler(process.env.NODE_ENV === 'production'))
