5e94fb2addd2c411df760180f8116294ed8c67d7
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-06-04 16:36:58 +0100

    EMP-291 Added test case

--- server/services/crmDisplayService.test.ts
+++ server/services/crmDisplayService.test.ts
@@ -114,6 +114,39 @@ describe('CRM Display Service', () => {
       })
     })
 
+    it('should return crm navigation for CaseDetails.cwCriminalProceeding=true', () => {
+      const customResponse: Crm5Response = {
+        ...crm5Response,
+        hasPreviousApplication: 'No',
+        CaseDetails: {
+          levelOfWork: 'Advocacy',
+          cwCriminalProceeding: true,
+          cwCriminalInvestigation: false,
+          cwCcrc: false,
+          cwAppealsReview: false,
+          cwPrisonLaw: true,
+        },
+      }
+      const result = crmDisplayService.getCrmNavigation('crm5', 1234567, '', customResponse)
+
+      expect(result).toEqual({
+        items: [
+          { href: '/crm5/1234567/general-information', text: 'General Information', active: true },
+          { href: '/crm5/1234567/firm-details', text: 'Firm Details', active: false },
+          { href: '/crm5/1234567/clients-details', text: "Client's Details", active: false },
+          { href: '/crm5/1234567/proceedings', text: 'Proceedings', active: false },
+          { href: '/crm5/1234567/statement-of-case', text: 'Statement of Case', active: false },
+          { href: '/crm5/1234567/solicitors-declaration', text: "Solicitor's Declaration", active: false },
+          { href: '/crm5/1234567/court-of-appeal-funding', text: 'Court of Appeal Funding', active: false },
+          { href: '/crm5/1234567/details-of-work-completed', text: 'Details of Work Completed', active: false },
+          { href: '/crm5/1234567/costs', text: 'Costs', active: false },
+          { href: '/crm5/1234567/case-history', text: 'Case History', active: false },
+          { href: '/crm5/1234567/solicitors-certification', text: "Solicitor's Certification", active: false },
+        ],
+        label: 'Side navigation',
+      })
+    })
+
     it('should return crm navigation for CaseDetails.levelOfWork=Advice', () => {
       const customResponse: Crm5Response = {
         ...crm5Response,
@@ -165,70 +198,6 @@ describe('CRM Display Service', () => {
           { href: '/crm5/1234567/clients-details', text: "Client's Details", active: false },
           { href: '/crm5/1234567/proceedings', text: 'Proceedings', active: false },
           { href: '/crm5/1234567/statement-of-case', text: 'Statement of Case', active: false },
-          { href: '/crm5/1234567/court-of-appeal-funding', text: 'Court of Appeal Funding', active: false },
-          { href: '/crm5/1234567/details-of-work-completed', text: 'Details of Work Completed', active: false },
-          { href: '/crm5/1234567/costs', text: 'Costs', active: false },
-          { href: '/crm5/1234567/case-history', text: 'Case History', active: false },
-          { href: '/crm5/1234567/solicitors-certification', text: "Solicitor's Certification", active: false },
-        ],
-        label: 'Side navigation',
-      })
-    })
-
-    it('should return crm navigation for CaseDetails.cwCriminalProceeding=true', () => {
-      const customResponse: Crm5Response = {
-        ...crm5Response,
-        CaseDetails: {
-          levelOfWork: 'Advocacy',
-          cwCriminalProceeding: true, // (hideWhen condition has precedence
-          cwCriminalInvestigation: false,
-          cwCcrc: false,
-          cwAppealsReview: false,
-          cwPrisonLaw: true,
-        },
-      }
-      const result = crmDisplayService.getCrmNavigation('crm5', 1234567, '', customResponse)
-
-      expect(result).toEqual({
-        items: [
-          { href: '/crm5/1234567/general-information', text: 'General Information', active: true },
-          { href: '/crm5/1234567/firm-details', text: 'Firm Details', active: false },
-          { href: '/crm5/1234567/clients-details', text: "Client's Details", active: false },
-          { href: '/crm5/1234567/proceedings', text: 'Proceedings', active: false },
-          { href: '/crm5/1234567/statement-of-case', text: 'Statement of Case', active: false },
-          { href: '/crm5/1234567/court-of-appeal-funding', text: 'Court of Appeal Funding', active: false },
-          { href: '/crm5/1234567/details-of-work-completed', text: 'Details of Work Completed', active: false },
-          { href: '/crm5/1234567/costs', text: 'Costs', active: false },
-          { href: '/crm5/1234567/case-history', text: 'Case History', active: false },
-          { href: '/crm5/1234567/solicitors-certification', text: "Solicitor's Certification", active: false },
-        ],
-        label: 'Side navigation',
-      })
-    })
-
-    it('should return crm navigation for hasPreviousApplication="No" and CaseDetails.cwCriminalProceeding=true', () => {
-      const customResponse: Crm5Response = {
-        ...crm5Response,
-        hasPreviousApplication: 'No', // (showWhen condition)
-        CaseDetails: {
-          levelOfWork: 'Advocacy',
-          cwCriminalProceeding: true, // (hideWhen condition has precedence
-          cwCriminalInvestigation: false,
-          cwCcrc: false,
-          cwAppealsReview: false,
-          cwPrisonLaw: true,
-        },
-      }
-      const result = crmDisplayService.getCrmNavigation('crm5', 1234567, '', customResponse)
-
-      expect(result).toEqual({
-        items: [
-          { href: '/crm5/1234567/general-information', text: 'General Information', active: true },
-          { href: '/crm5/1234567/firm-details', text: 'Firm Details', active: false },
-          { href: '/crm5/1234567/clients-details', text: "Client's Details", active: false },
-          { href: '/crm5/1234567/proceedings', text: 'Proceedings', active: false },
-          { href: '/crm5/1234567/statement-of-case', text: 'Statement of Case', active: false },
-          { href: '/crm5/1234567/solicitors-declaration', text: "Solicitor's Declaration", active: false },
           { href: '/crm5/1234567/court-of-appeal-funding', text: 'Court of Appeal Funding', active: false },
           { href: '/crm5/1234567/details-of-work-completed', text: 'Details of Work Completed', active: false },
           { href: '/crm5/1234567/costs', text: 'Costs', active: false },
@@ -308,6 +277,97 @@ describe('CRM Display Service', () => {
         sectionId: 'clients-details',
         subsections: [{ fields: [{ subHeading: 'Address' }], title: "Client's Details" }],
         title: "Client's Details",
+      })
+    })
+
+    it('should return crm section if showWhen condition met', () => {
+      const customResponse: Crm5Response = {
+        ...crm5Response,
+        hasPreviousApplication: 'No',
+      }
+      const result = crmDisplayService.getCrmSection('crm5', 'capital-details', customResponse)
+
+      expect(result).toEqual({
+        hideWhen: {
+          apiField: 'CaseDetails.cwCriminalProceeding',
+          equals: 'true',
+        },
+        sectionId: 'capital-details',
+        showWhen: {
+          apiField: 'hasPreviousApplication',
+          equals: 'No',
+        },
+        subsections: [
+          {
+            fields: [
+              {
+                label: 'Is your client under 18 years old?',
+                value: 'No',
+              },
+              {
+                label:
+                  "Does your client or partner (if living with client as couple) get Income Support, Income Based Job Seeker's Allowance, Income Related Employment and Support Allowance or Guarantee State Pension Credit?",
+                value: 'Yes',
+              },
+              {
+                label: 'How many dependants does your client have?',
+                value: 2,
+              },
+              {
+                label: 'Client',
+                type: 'currency',
+                value: 60000,
+              },
+              {
+                label: 'Partner',
+                type: 'currency',
+                value: 40000,
+              },
+              {
+                label: 'Total',
+                type: 'currency',
+                value: 100000,
+              },
+            ],
+            title: 'Capital Details',
+          },
+        ],
+        title: 'Capital Details',
+      })
+    })
+
+    it('should return first section if hideWhen condition met', () => {
+      const customResponse: Crm5Response = {
+        ...crm5Response,
+        CaseDetails: {
+          levelOfWork: 'Advocacy',
+          cwCriminalProceeding: true,
+          cwCriminalInvestigation: false,
+          cwCcrc: false,
+          cwAppealsReview: false,
+          cwPrisonLaw: true,
+        },
+      }
+      const result = crmDisplayService.getCrmSection('crm5', 'capital-details', customResponse)
+
+      expect(result).toEqual({
+        sectionId: 'general-information',
+        subsections: [
+          {
+            fields: [
+              { label: 'Has a previous application for an extension been made?', value: 'Yes' },
+              {
+                label:
+                  'Have you successfully appealed a previous decision of a CRM5 application (for the same matter)?',
+                value: 'No',
+              },
+              { label: 'Urgent?', value: 'Yes' },
+              { label: 'Reason for urgency', value: 'Urgent' },
+            ],
+            title: 'General Information',
+          },
+        ],
+        title: 'General Information',
       })
     })
 
