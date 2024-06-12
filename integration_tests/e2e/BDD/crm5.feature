714577200973340e9c0c72762953c43ec4748316
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-06-12 13:38:48 +0100

    created BDD tests for crm 5

--- integration_tests/e2e/BDD/crm5.feature
+++ integration_tests/e2e/BDD/crm5.feature
@@ -4,3 +4,8 @@ Feature: CRM5 Case Navigation
     Given the user is on the Historical Data page
     When the user selects "Historical data eForm" link
     And the user selects "CRM5" from the CRM type dropdown and clicks search
+    And the user clicks on a Client name or USN link
+    Then it should return a page related to the corresponding CRM5 case
+    And we should see a left bar navigation with 12 items
+    And we should see a right container with static infos displayed
+    And the Costs page should be displayed with the correct layout
--- integration_tests/e2e/BDD/crm5.ts
+++ integration_tests/e2e/BDD/crm5.ts
@@ -1,7 +1,7 @@
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 
 Given('the user is on the Historical Data page', () => {
-  cy.visit('/') // Adjust the URL to your historical data page
+  cy.visit('/')
 })
 
 When('the user selects "Historical data eForm" link', () => {
@@ -11,4 +11,25 @@ When('the user selects "Historical data eForm" link', () => {
 When('the user selects "CRM5" from the CRM type dropdown and clicks search', () => {
   cy.get('select[name="type"]').select('CRM 5')
   cy.get('button[type="submit"]').click()
+})
+
+When('the user clicks on a Client name or USN link', () => {
+  cy.get('table.govuk-table tbody tr td.govuk-table__cell  a').first().click()
+})
+Then('it should return a page related to the corresponding CRM5 case', () => {
+  cy.url().should('include', '/crm5/')
+})
+
+Then('we should see a left bar navigation with 12 items', () => {
+  cy.get('.moj-side-navigation__list').children().should('have.length', 12) // Adjust selector if needed
+})
+
+Then('we should see a right container with static infos displayed', () => {
+  cy.get('.govuk-main-wrapper').should('be.visible')
+  cy.get('.govuk-main-wrapper').contains('General Information')
+})
+
+Then('the Costs page should be displayed with the correct layout', () => {
+  cy.contains('Costs').click()
+  cy.contains('Costs Accrued To Date').should('be.visible')
 })
