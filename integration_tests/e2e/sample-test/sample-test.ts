22e4ef240fcdcde2a4e426413f4a0d0ca4fbf3f7
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-06-13 14:23:38 +0100

    deleted test folders

--- integration_tests/e2e/sample-test/sample-test.ts
+++
@@ -1,9 +0,0 @@
-import { When, Then } from '@badeball/cypress-cucumber-preprocessor'
-
-When('I visit duckduckgo.com', () => {
-  cy.visit('https://www.duckduckgo.com')
-})
-
-Then('I should see a search bar', () => {
-  cy.get('input').should('exist')
-})
