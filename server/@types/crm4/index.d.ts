e0f74f72eb77dd9e1dff71e8a2e04cc5144be266
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-06-19 16:45:23 +0100

    EMP-361 Updated types and test data

--- server/@types/crm4/index.d.ts
+++ server/@types/crm4/index.d.ts
@@ -1,132 +1,111 @@
 import { CrmResponse } from '@eqApi'
 
-type Firm = {
-  urn: string
-  firmAddress: string
-  firmName: string
-  firmPhone: string
-  firmSupplierName: string
-  firmSupplierNo: string
-  firmContactName: string
-  firmSolicitorName: string
-}
-
-type SolicitorDetails = {
-  solicitorName: string
-  solicitorReference: string
-}
-
-type ClientDetails = {
-  ufn: string
-  maatNumber: string
-  firstName: string
-  surname: string
-  dateOfBirth: string
-  prisonLaw: string
-}
-
-type ProceedingDetails = {
-  isClientDetained: string
-  detainedDetails: string
-  courtType: string
-  psychiatricLiaison: string
-  psychiatricDetails: string
-  mainOffence: string
-  actualPlea: string
-  dateOfHearing: string
-}
-
-type CaseDetails = {
-  isPOCA: string
-  Firm: Firm
-  SolicitorDetails: SolicitorDetails
-  ClientDetails: ClientDetails
-  ProceedingDetails: ProceedingDetails
-}
-
-type Details = {
-  expenditureType: string
-  priorAuthority: string
-  expertName: string
-  companyName: string
-  statusExpert: string
-  postCodeExpert: string
-}
-
-type Preparation = {
-  hours: string
-  hourlyRate: number
-  total: number
-}
-
-type Travel = {
-  hours: string
-  rate: number
-  total: number
-}
-
-type AdditionalExpenditure = {
-  description: string
-  justification: string
-  quantity: number
-  rate: number
-  total: number
-}
-
-type ExpenditureDetails = {
-  Details: Details
-  Preparation: Preparation
-  AdditionalExpenditure: Array<AdditionalExpenditure>
-  Travel: Travel
-  Authority: number
-}
-
-type Quote = {
-  companyName: string
-  expertName: string
-  contactPhone: string
-  costBasis: string
-  preparationHours: number
-  hourlyRate: number
-  additionalItemDesc: string
-  additionalItemAmount: number
-  travelHours: string
-  travelHourlyRate: number
-  quoteTotal: number
-  qcDetails: string
-}
-
-type AlternativeQuotes = {
-  alternativeQuote: string
-  reason: string
-  numberOfQuotes: number
-  Quotes: Array<Quote>
-}
-
-type PriorAuthorityDetails = {
-  authorityDetails: string
-  prosecutionSummary: string
-  defenceMitigation: string
-  qcDetails: string
-}
-
-type Certification = {
-  date: string
-  name: string
-}
-
-type Solicitor = {
-  declaration: true
-  certification: Certification
-}
-
 export interface Crm4Response extends CrmResponse {
-  greaterValue: boolean
-  postMortemExamination: string
-  CaseDetails?: CaseDetails
-  ExpenditureDetails?: ExpenditureDetails
-  AlternativeQuotes?: AlternativeQuotes
-  PriorAuthorityDetails?: PriorAuthorityDetails
-  AdditionalInfo?: string
-  Solicitor?: Solicitor
+  formDetails: {
+    greaterValue: boolean
+    postMortemExamination: string
+    CaseDetails?: {
+      isPOCA: string
+      Firm: {
+        urn: string
+        firmAddress: string
+        firmName: string
+        firmPhone: string
+        firmSupplierName: string
+        firmSupplierNo: string
+        firmContactName: string
+        firmSolicitorName: string
+      }
+      SolicitorDetails: {
+        solicitorName: string
+        solicitorReference: string
+      }
+      ClientDetails: {
+        ufn: string
+        maatNumber: string
+        firstName: string
+        surname: string
+        dateOfBirth: string
+        prisonLaw: string
+      }
+      ProceedingDetails: {
+        isClientDetained: string
+        detainedDetails: string
+        courtType: string
+        psychiatricLiaison: string
+        psychiatricDetails: string
+        mainOffence: string
+        actualPlea: string
+        dateOfHearing: string
+      }
+    }
+    ExpenditureDetails?: {
+      Details: {
+        expenditureType: string
+        priorAuthority: string
+        expertName: string
+        companyName: string
+        statusExpert: string
+        postCodeExpert: string
+      }
+      Preparation: {
+        hours: string
+        hourlyRate: number
+        total: number
+      }
+      AdditionalExpenditure: [
+        {
+          description: string
+          justification: string
+          quantity: number
+          rate: number
+          total: number
+        },
+      ]
+      Travel: {
+        hours: string
+        rate: number
+        total: number
+      }
+      Authority: number
+    }
+    AlternativeQuotes?: {
+      alternativeQuote: string
+      reason: string
+      numberOfQuotes: number
+      Quotes: [
+        {
+          companyName: string
+          expertName: string
+          contactPhone: string
+          costBasis: string
+          preparationHours: number
+          hourlyRate: number
+          additionalItemDesc: string
+          additionalItemAmount: number
+          travelHours: string
+          travelHourlyRate: number
+          quoteTotal: number
+          qcDetails: string
+        },
+      ]
+    }
+    PriorAuthorityDetails?: {
+      authorityDetails: string
+      prosecutionSummary: string
+      defenceMitigation: string
+      qcDetails: string
+    }
+    AdditionalInfo?: string
+    Solicitor?: {
+      declaration: true
+      certification: {
+        date: string
+        name: string
+      }
+    }
+    OfficeUseOnly?: {
+      decision: string
+    }
+  }
 }
--- server/@types/crm5/index.d.ts
+++ server/@types/crm5/index.d.ts
@@ -1,104 +1,5 @@
 import { CrmResponse } from '@eqApi'
 
-type Firm = {
-  firmAddress: string
-  firmName: string
-  firmPhone: string
-  firmSupplierNo: string
-  firmContactName: string
-  firmSolicitorName: string
-  firmSolicitorRef: string
-}
-
-type CaseDetails = {
-  levelOfWork: string
-  cwCriminalProceeding: boolean
-  cwCriminalInvestigation: boolean
-  cwCcrc: boolean
-  cwAppealsReview: boolean
-  cwPrisonLaw: boolean
-}
-
-type ClientDetails = {
-  firstName: string
-  middleName: string
-  surname: string
-  maritalStatus: string
-  dateOfBirth: string
-  nationalInsuranceNumber: string
-  address: {
-    noFixedAbode: boolean
-    postcode: string
-    addressLine1: string
-    addressLine2: string
-    addressLine3: string
-    city: string
-    county: string
-    country: string
-  }
-  UFN: string
-}
-
-type LaaAdviceAssistance = {
-  providedAdvice: string
-  notes: string
-}
-
-type AdviceAssistance = {
-  transferFromSolicitor: string
-  adviceCriteria: string
-  laaAdviceAssistance: LaaAdviceAssistance
-}
-
-type CapitalDetails = {
-  isUnder18: string
-  hasIncomeSupport: string
-  numOfDependants: number
-  clientSavings: number
-  partnerSavings: number
-  totalSavings: number
-}
-
-type IncomeDetails = {
-  hasIncomeSupport: boolean
-  weeklyClientIncome: number
-  weeklyPartnerIncome: number
-  weeklyIncomeWithoutDeduction: number
-  incomeTaxDeductions: number
-  niDeductions: number
-  partnerDeductions: number
-  socialFundDeductions: number
-  dependantChildrenUnder15: number
-  deductionUnder15: number
-  dependantChildrenOver16: number
-  deductionOver16: number
-  totalDeductions: number
-  totalWeeklyIncome: number
-}
-
-type TypeOfProceedings = {
-  preCharge: string
-  appealProceedings: string
-  prisonLaw: string
-}
-
-type DetailsOfProceedings = {
-  dateOfNextHearing: string
-  isCounselInstructed: string
-}
-
-type Proceedings = {
-  TypeOfProceedings: TypeOfProceedings
-  DetailsOfProceedings: DetailsOfProceedings
-}
-
-type CourtAppealFunding = {
-  hasCourtAppeal: string
-  appealDetails: string
-  benefitOfCourt: string
-  expertReport: string
-}
-
 export type TimeAndCost = {
   time: string
   cost: number
@@ -113,78 +14,145 @@ type Cost = {
   cost: number
 }
 
-type AccruedCosts = {
-  Attendance: TimeAndCost
-  Preparation: TimeAndCost
-  Advocacy: TimeAndCost
-  Travel: TimeAndCost
-  Waiting: TimeAndCost
-  Letters: TotalAndCost
-  TelephoneCalls: TotalAndCost
-  Mileage: TotalAndCost
-  OtherDisbursement: Cost
-  TotalCost: Cost
-}
-
-type AnticipatedCosts = {
-  Attendance: TimedAndCost
-  Preparation: TimedAndCost
-  Advocacy: TimedAndCost
-  Travel: TimedAndCost
-  Waiting: TimedAndCost
-  Letters: TimedAndCost
-  TelephoneCalls: TimedAndCost
-  Mileage: TimedAndCost
-  OtherDisbursement: Cost
-  TotalCost: Cost
-}
-
-type AllCosts = {
-  AccruedCosts: AccruedCosts
-  AnticipatedCosts: AnticipatedCosts
-}
-
-type CaseHistory = {
-  summary: string
-  additionalInfo: string
-}
-
-type Solicitor = {
-  declaration: {
-    date: string
-    name: string
-  }
-  certification: {
-    date: string
-    name: string
-  }
-}
-
-type OfficeUseOnly = {
-  decision: string
-}
-
 export interface Crm5Response extends CrmResponse {
-  usn: number
-  hasPreviousApplication: string
-  previousApplicationRef: string
-  appealedPrevDecision: string
-  appealedPrevDecisionDetails: string
-  urgent: string
-  urgencyReason: string
-  Firm?: Firm
-  CaseDetails?: CaseDetails
-  ClientDetails?: ClientDetails
-  AdviceAssistance?: AdviceAssistance
-  CapitalDetails?: CapitalDetails
-  IncomeDetails?: IncomeDetails
-  Proceedings?: Proceedings
-  CourtAppealFunding?: CourtAppealFunding
-  AllCosts?: AllCosts
-  CaseHistory?: CaseHistory
-  Solicitor?: Solicitor
-  OfficeUseOnly?: OfficeUseOnly
-  StatementOfCase: string
-  DetailsOfWorkCompleted: string
-  DetailsOfApplication: string
+  formDetails: {
+    usn: number
+    hasPreviousApplication: string
+    previousApplicationRef: string
+    appealedPrevDecision: string
+    appealedPrevDecisionDetails: string
+    urgent: string
+    urgencyReason: string
+    Firm?: {
+      firmAddress: string
+      firmName: string
+      firmPhone: string
+      firmSupplierNo: string
+      firmContactName: string
+      firmSolicitorName: string
+      firmSolicitorRef: string
+    }
+    CaseDetails?: {
+      levelOfWork: string
+      cwCriminalProceeding: boolean
+      cwCriminalInvestigation: boolean
+      cwCcrc: boolean
+      cwAppealsReview: boolean
+      cwPrisonLaw: boolean
+    }
+    ClientDetails?: {
+      firstName: string
+      middleName: string
+      surname: string
+      maritalStatus: string
+      dateOfBirth: string
+      nationalInsuranceNumber: string
+      address: {
+        noFixedAbode: boolean
+        postcode: string
+        addressLine1: string
+        addressLine2: string
+        addressLine3: string
+        city: string
+        county: string
+        country: string
+      }
+      UFN: string
+    }
+    AdviceAssistance?: {
+      transferFromSolicitor: string
+      adviceCriteria: string
+      laaAdviceAssistance: {
+        providedAdvice: string
+        notes: string
+      }
+    }
+    CapitalDetails?: {
+      isUnder18: string
+      hasIncomeSupport: string
+      numOfDependants: number
+      clientSavings: number
+      partnerSavings: number
+      totalSavings: number
+    }
+    IncomeDetails?: {
+      hasIncomeSupport: boolean
+      weeklyClientIncome: number
+      weeklyPartnerIncome: number
+      weeklyIncomeWithoutDeduction: number
+      incomeTaxDeductions: number
+      niDeductions: number
+      partnerDeductions: number
+      socialFundDeductions: number
+      dependantChildrenUnder15: number
+      deductionUnder15: number
+      dependantChildrenOver16: number
+      deductionOver16: number
+      totalDeductions: number
+      totalWeeklyIncome: number
+    }
+    Proceedings?: {
+      TypeOfProceedings: {
+        preCharge: string
+        appealProceedings: string
+        prisonLaw: string
+      }
+      DetailsOfProceedings: {
+        dateOfNextHearing: string
+        isCounselInstructed: string
+      }
+    }
+    CourtAppealFunding?: {
+      hasCourtAppeal: string
+      appealDetails: string
+      benefitOfCourt: string
+      expertReport: string
+    }
+    AllCosts?: {
+      AccruedCosts: {
+        Attendance: TimeAndCost
+        Preparation: TimeAndCost
+        Advocacy: TimeAndCost
+        Travel: TimeAndCost
+        Waiting: TimeAndCost
+        Letters: TotalAndCost
+        TelephoneCalls: TotalAndCost
+        Mileage: TotalAndCost
+        OtherDisbursement: Cost
+        TotalCost: Cost
+      }
+      AnticipatedCosts: {
+        Attendance: TimedAndCost
+        Preparation: TimedAndCost
+        Advocacy: TimedAndCost
+        Travel: TimedAndCost
+        Waiting: TimedAndCost
+        Letters: TimedAndCost
+        TelephoneCalls: TimedAndCost
+        Mileage: TimedAndCost
+        OtherDisbursement: Cost
+        TotalCost: Cost
+      }
+    }
+    CaseHistory?: {
+      summary: string
+      additionalInfo: string
+    }
+    Solicitor?: {
+      declaration: {
+        date: string
+        name: string
+      }
+      certification: {
+        date: string
+        name: string
+      }
+    }
+    OfficeUseOnly?: {
+      decision: string
+    }
+    StatementOfCase: string
+    DetailsOfWorkCompleted: string
+    DetailsOfApplication: string
+  }
 }
