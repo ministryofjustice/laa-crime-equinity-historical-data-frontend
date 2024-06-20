8f9618b6b4dc3d11257a623f824b1cb5d60bea73
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-06-20 11:41:29 +0100

    created backlink.njk and asdded to crmDetails.njk added history, searchResults and formValues types to session data included history, searchResults and formValues in show function of searchEformController.ts added back button logic to crm5Controller.ts updated tests merged main into branch PR Changes + additional test for back link put backlink function into separate utils file

--- server/controllers/crm/crm4Controller.ts
+++ server/controllers/crm/crm4Controller.ts
@@ -1,7 +1,57 @@
-import type { Request, RequestHandler, Response } from 'express'
+// import type { Request, RequestHandler, Response } from 'express'
+// import { Crm4Response } from '@crm4'
+// import CrmApiService from '../../services/crmApiService'
+// import CrmDisplayService from '../../services/crmDisplayService'
+//
+// export default class Crm4Controller {
+//   constructor(
+//     private readonly crm4Service: CrmApiService<Crm4Response>,
+//     private readonly crmDisplayService: CrmDisplayService,
+//   ) {}
+//
+//   show(): RequestHandler {
+//     return async (req: Request, res: Response): Promise<void> => {
+//       const usn = Number(req.params.usn)
+//       const { sectionId } = req.params
+//       const crm4Response = await this.crm4Service.getCrm(usn)
+//       const navigation = this.crmDisplayService.getCrmNavigation('crm4', usn, sectionId, crm4Response)
+//       const section = this.crmDisplayService.getCrmSection('crm4', sectionId, crm4Response)
+//
+//       const history = req.session.history || []
+//       const currentUrl = `/crm4/${usn}/${sectionId || ''}`
+//
+//       if (req.query.fromBack) {
+//         if (history.length > 1) {
+//           history.pop()
+//         }
+//       } else if (history.length === 0 || history[history.length - 1] !== currentUrl) {
+//         history.push(currentUrl)
+//       }
+//
+//       req.session.history = history
+//
+//       let backUrl = '/search-eform'
+//       if (history.length > 1) {
+//         backUrl = `${history[history.length - 2]}?fromBack=true`
+//       }
+//
+//       res.render('pages/crmDetails', {
+//         title: 'Application for Prior Authority to Incur Disbursements in Criminal Cases',
+//         navigationItems: navigation,
+//         usn,
+//         crmType: 'CRM 4',
+//         section,
+//         backUrl,
+//       })
+//     }
+//   }
+// }
+
+import type { RequestHandler } from 'express'
 import { Crm4Response } from '@crm4'
 import CrmApiService from '../../services/crmApiService'
 import CrmDisplayService from '../../services/crmDisplayService'
+import manageBackLink from '../../utils/crmBackLink'
 
 export default class Crm4Controller {
   constructor(
@@ -10,30 +60,15 @@ export default class Crm4Controller {
   ) {}
 
   show(): RequestHandler {
-    return async (req: Request, res: Response): Promise<void> => {
+    return async (req, res) => {
       const usn = Number(req.params.usn)
       const { sectionId } = req.params
       const crm4Response = await this.crm4Service.getCrm(usn)
       const navigation = this.crmDisplayService.getCrmNavigation('crm4', usn, sectionId, crm4Response)
       const section = this.crmDisplayService.getCrmSection('crm4', sectionId, crm4Response)
 
-      const history = req.session.history || []
-      const currentUrl = `/crm4/${usn}/${sectionId || ''}`
-
-      if (req.query.fromBack) {
-        if (history.length > 1) {
-          history.pop()
-        }
-      } else if (history.length === 0 || history[history.length - 1] !== currentUrl) {
-        history.push(currentUrl)
-      }
-
-      req.session.history = history
-
-      let backUrl = '/search-eform'
-      if (history.length > 1) {
-        backUrl = `${history[history.length - 2]}?fromBack=true`
-      }
+      const currentUrl = `/crm4/${usn}/${sectionId || 'general-information'}`
+      const backUrl = manageBackLink(req, currentUrl)
 
       res.render('pages/crmDetails', {
         title: 'Application for Prior Authority to Incur Disbursements in Criminal Cases',
--- server/controllers/crm/crm5Controller.ts
+++ server/controllers/crm/crm5Controller.ts
@@ -1,7 +1,57 @@
-import type { Request, RequestHandler, Response } from 'express'
+// import type { Request, RequestHandler, Response } from 'express'
+// import { Crm5Response } from '@crm5'
+// import CrmApiService from '../../services/crmApiService'
+// import CrmDisplayService from '../../services/crmDisplayService'
+//
+// export default class Crm5Controller {
+//   constructor(
+//     private readonly crm5Service: CrmApiService<Crm5Response>,
+//     private readonly crmDisplayService: CrmDisplayService,
+//   ) {}
+//
+//   show(): RequestHandler {
+//     return async (req: Request, res: Response): Promise<void> => {
+//       const usn = Number(req.params.usn)
+//       const { sectionId } = req.params
+//       const crm5Response = await this.crm5Service.getCrm(usn)
+//       const navigation = this.crmDisplayService.getCrmNavigation('crm5', usn, sectionId, crm5Response)
+//       const section = this.crmDisplayService.getCrmSection('crm5', sectionId, crm5Response)
+//
+//       const history = req.session.history || []
+//       const currentUrl = `/crm5/${usn}/${sectionId || ''}`
+//
+//       if (req.query.fromBack) {
+//         if (history.length > 1) {
+//           history.pop()
+//         }
+//       } else if (history.length === 0 || history[history.length - 1] !== currentUrl) {
+//         history.push(currentUrl)
+//       }
+//
+//       req.session.history = history
+//
+//       let backUrl = '/search-eform'
+//       if (history.length > 1) {
+//         backUrl = `${history[history.length - 2]}?fromBack=true`
+//       }
+//
+//       res.render('pages/crmDetails', {
+//         title: 'Application For Extension Of Upper Limit',
+//         navigationItems: navigation,
+//         usn,
+//         crmType: 'CRM 5',
+//         section,
+//         backUrl,
+//       })
+//     }
+//   }
+// }
+
+import type { RequestHandler } from 'express'
 import { Crm5Response } from '@crm5'
 import CrmApiService from '../../services/crmApiService'
 import CrmDisplayService from '../../services/crmDisplayService'
+import manageBackLink from '../../utils/crmBackLink'
 
 export default class Crm5Controller {
   constructor(
@@ -10,30 +60,15 @@ export default class Crm5Controller {
   ) {}
 
   show(): RequestHandler {
-    return async (req: Request, res: Response): Promise<void> => {
+    return async (req, res) => {
       const usn = Number(req.params.usn)
       const { sectionId } = req.params
       const crm5Response = await this.crm5Service.getCrm(usn)
       const navigation = this.crmDisplayService.getCrmNavigation('crm5', usn, sectionId, crm5Response)
       const section = this.crmDisplayService.getCrmSection('crm5', sectionId, crm5Response)
 
-      const history = req.session.history || []
-      const currentUrl = `/crm5/${usn}/${sectionId || ''}`
-
-      if (req.query.fromBack) {
-        if (history.length > 1) {
-          history.pop()
-        }
-      } else if (history.length === 0 || history[history.length - 1] !== currentUrl) {
-        history.push(currentUrl)
-      }
-
-      req.session.history = history
-
-      let backUrl = '/search-eform'
-      if (history.length > 1) {
-        backUrl = `${history[history.length - 2]}?fromBack=true`
-      }
+      const currentUrl = `/crm5/${usn}/${sectionId || 'general-information'}`
+      const backUrl = manageBackLink(req, currentUrl)
 
       res.render('pages/crmDetails', {
         title: 'Application For Extension Of Upper Limit',
