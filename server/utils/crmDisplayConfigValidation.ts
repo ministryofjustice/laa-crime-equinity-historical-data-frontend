5ea05163db460d086d22b320be92bd51cb66520b
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-06-21 14:33:44 +0100

    EMP-355 Fixed field formats

--- server/utils/crmDisplayConfigValidation.ts
+++ server/utils/crmDisplayConfigValidation.ts
@@ -23,7 +23,7 @@ const schema = Joi.object({
             {
               label: Joi.string().optional().allow(''),
               apiField: Joi.string().required(),
-              type: Joi.string().valid('currency', 'date', 'time', 'timeAndCost', 'totalAndCost').optional(),
+              type: Joi.string().valid('currency', 'date', 'time', 'percent', 'timeAndCost', 'totalAndCost').optional(),
             },
             {
               subHeading: Joi.string().required(),
