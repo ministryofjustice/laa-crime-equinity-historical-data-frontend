75f38e116d4d4daac1996e9215e8f1cd1b7fe401
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-07-09 11:39:48 +0100

    EMP-388 additional logging

--- server/utils/userProfileGroups.ts
+++ server/utils/userProfileGroups.ts
@@ -1,12 +1,14 @@
 import type { Response } from 'express'
 
+import logger from '../../logger'
+
 import config from '../config'
 
 function getProfileAcceptedTypes(res: Response): string {
   const ssoUserGroups = res.locals.ssoUserGroups || []
   const allowedUserProfileGroups = config.sso.allowedUserProfileGroups.split(',')
 
-  return allowedUserProfileGroups
+  const profileAcceptedTypes = allowedUserProfileGroups
     .map(group => {
       const [groupId, crmTypeId] = group.split(':')
       if (ssoUserGroups.includes(groupId)) {
@@ -16,6 +18,9 @@ function getProfileAcceptedTypes(res: Response): string {
     })
     .filter(Boolean)
     .join(',')
+
+  logger.info(`profileAcceptedTypes:${profileAcceptedTypes}`)
+  return profileAcceptedTypes
 }
 
 export default getProfileAcceptedTypes
