6d587530c50e1a5589088bbf880cd6889034e884
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-07-17 16:33:22 +0100

    EMP-406 Added test

--- server/auth/authProvider.test.ts
+++ server/auth/authProvider.test.ts
@@ -1,7 +1,8 @@
 import nock from 'nock'
 import { createMock, DeepMocked } from '@golevelup/ts-jest'
-import type { Request, Response } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 import { Session } from 'express-session'
+import { AuthorizationCodeRequest } from '@azure/msal-node/src/request/AuthorizationCodeRequest'
 import config from '../config'
 
 import authProvider from './authProvider'
@@ -12,6 +13,7 @@ describe('authProvider', () => {
   let request: DeepMocked<Request>
   let response: DeepMocked<Response>
   let session: DeepMocked<Session>
+  const next: DeepMocked<NextFunction> = createMock<NextFunction>({})
 
   beforeEach(() => {
     request = createMock<Request>({})
@@ -22,6 +24,34 @@ describe('authProvider', () => {
 
   afterEach(() => {
     nock.cleanAll()
+  })
+
+  it('should handle redirect request', async () => {
+    fakeAuthClient.post('/oauth2/v2.0/token').reply(200, {
+      access_token: 'some-access-token',
+    })
+
+    const requestHandler = authProvider.handleRedirect()
+
+    request.session = session
+    request.session.pkceCodes = {
+      verifier: 'some-verifier',
+      challenge: 'some-challenge',
+      challengeMethod: 'some-challengeMethod',
+    }
+    request.session.authCodeRequest = {
+      redirectUri: '/oauth2/v2.0/token',
+    } as AuthorizationCodeRequest
+    request.session.tokenCache = null
+
+    request.body = {
+      code: 'someCode',
+      state: 'eyJzdWNjZXNzUmVkaXJlY3QiOiIvIn0=', // base64 encoded {"successRedirect":"/"}
+    }
+
+    await requestHandler(request, response, next)
+
+    expect(response.redirect).toHaveBeenCalledWith('/')
   })
 
   it('should handle logout request', async () => {
