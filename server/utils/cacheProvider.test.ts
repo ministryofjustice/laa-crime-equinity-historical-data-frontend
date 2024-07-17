31570de1678934aacee2eedd1e46666a155a6825
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-07-17 14:32:57 +0100

    EMP-406 Add SDS_AUTH_SCOPE to deployment.yml

--- server/utils/cacheProvider.test.ts
+++ server/utils/cacheProvider.test.ts
@@ -11,13 +11,13 @@ describe('CacheProvider', () => {
     expect(result).toEqual('123456')
   })
 
-  it('should return true if item in cache', () => {
+  it('should return true if key exists in cache', () => {
     const result = sdsAuthCache.has('some-key')
 
     expect(result).toBe(true)
   })
 
-  it('should return false if item is not in  cache', () => {
+  it('should return false if key does not exist in cache', () => {
     const result = sdsAuthCache.has('???')
 
     expect(result).toBe(false)
