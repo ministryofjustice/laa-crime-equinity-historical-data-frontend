df8b8ab356c21ded9947f9e4462cbb2f6030b158
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-07-29 13:24:04 +0100

    created crm14Income.njk amended crm14DisplayConfig.json amended customDisplay.njk added test api data from crm14Controller.ts

--- server/controllers/crm/crm14Controller.ts
+++ server/controllers/crm/crm14Controller.ts
@@ -15,7 +15,83 @@ export default class Crm14Controller {
     return async (req: Request, res: Response): Promise<void> => {
       const usn = Number(req.params.usn)
       const { sectionId } = req.params
-      const crm14Response = await this.crm14Service.getCrm(usn, getProfileAcceptedTypes(res))
+      // const crm14Response = await this.crm14Service.getCrm(usn, getProfileAcceptedTypes(res))
+      const crm14Response = {
+        formDetails: {
+          income: {
+            receiveBenefits: 'No',
+            benefits: {
+              you: {
+                incomeSupport: false,
+                esa: false,
+                statePension: true, // Example value
+                jsa: false,
+              },
+              partner: {
+                incomeSupport: false,
+                esa: false,
+                statePension: true, // Example value
+                jsa: false,
+              },
+            },
+            proofBenefits: '',
+            selfEmployed: 'No',
+            totalIncome: 'No',
+            sourcesOfIncome: {
+              you: {
+                employment: { amount: '2000', frequency: 'Monthly' },
+                childBenefit: { amount: '500', frequency: 'Monthly' },
+                workingTaxCredits: { amount: '300', frequency: 'Monthly' },
+                universalCredit: { amount: '400', frequency: 'Monthly' },
+                otherBenefits: { amount: '100', frequency: 'Monthly' },
+                maintenanceIncome: { amount: '200', frequency: 'Monthly' },
+                pensions: { amount: '150', frequency: 'Monthly' },
+                otherSource: {
+                  amount: '100',
+                  frequency: 'Monthly',
+                  studentGrant: true,
+                  moneyFromFriends: false,
+                  maintenance: false,
+                  boardOrRent: false,
+                  rentalIncome: false,
+                  financialSupport: false,
+                  other: 'Inheritance',
+                },
+              },
+              partner: {
+                employment: { amount: '1800', frequency: 'Monthly' },
+                childBenefit: { amount: '450', frequency: 'Monthly' },
+                workingTaxCredits: { amount: '280', frequency: 'Monthly' },
+                universalCredit: { amount: '380', frequency: 'Monthly' },
+                otherBenefits: { amount: '90', frequency: 'Monthly' },
+                maintenanceIncome: { amount: '180', frequency: 'Monthly' },
+                pensions: { amount: '140', frequency: 'Monthly' },
+                otherSource: {
+                  amount: '80',
+                  frequency: 'Monthly',
+                  studentGrant: false,
+                  moneyFromFriends: true,
+                  maintenance: false,
+                  boardOrRent: false,
+                  rentalIncome: false,
+                  financialSupport: false,
+                  other: '',
+                },
+              },
+            },
+            restraintOrder: 'No',
+            ownProperty: 'No',
+            savingsOrInvestments: 'No',
+            expensesExplanation: {
+              sleepingOnSofa: false,
+              stayingWithFamily: false,
+              livingOnStreets: true,
+              inCustody: false,
+              other: 'lottery winnings',
+            },
+          },
+        },
+      }
       const navigation = this.crmDisplayService.getNavigation('crm14', usn, sectionId, crm14Response)
       const sections = this.crmDisplayService.getSections('crm14', sectionId, crm14Response)
 
