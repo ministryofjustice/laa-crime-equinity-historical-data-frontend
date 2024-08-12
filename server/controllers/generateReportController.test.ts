1f1f51e3dab37cecddc398dfd1bb41927db850b7
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-08-12 17:11:13 +0100

    EMP-434 Fixed tests

--- server/controllers/generateReportController.test.ts
+++ server/controllers/generateReportController.test.ts
@@ -9,7 +9,7 @@ jest.mock('../utils/userProfileGroups', () => {
   return jest.fn().mockReturnValue('1,4,5,6')
 })
 
-xdescribe('downloadEvidenceController', () => {
+describe('generateReportController', () => {
   let mockCrmReportApiService: jest.Mocked<CrmReportApiService>
   let request: DeepMocked<Request>
   let response: DeepMocked<Response>
@@ -27,19 +27,18 @@ xdescribe('downloadEvidenceController', () => {
 
     await requestHandler(request, response, next)
 
-    expect(response.render).toHaveBeenCalledWith('pages/generateReport')
+    expect(response.render).toHaveBeenCalledWith('pages/generateReport', { backUrl: '/' })
   })
 
-  it('should download the requested CRM file', async () => {
-    mockCrmReportApiService.getCrmReport.mockResolvedValue(successResponse())
+  it('should download the requested CRM report', async () => {
+    const crmReportResponse = getCrmReportResponse()
 
-    const downloadData =
-      ' `Client UFN,Usn,Provider Account,Firm Name,Client Name,Rep Order Number,Maat ID,Prison Law,Date Received,Decision Date,Decision,Expenditure Type,Expert Name,Quantity,Rate,Unit,Total Cost,Additional Expenditure,Total Authority,Total Granted,Granting Caseworker\n' +
-      '        031022/777,5001613,0D182J,ABELS,Joe modo,78543657,,No,2023-03-16,2023-03-16,Grant,a Psychiatrist,tyjtjtjt,4.0,50.0,Hour(s),200.0,0.0,200.0,200.0,Sym-G`'
+    mockCrmReportApiService.getCrmReport.mockResolvedValue(crmReportResponse)
 
     const generateReportController = new GenerateReportController(mockCrmReportApiService)
     const requestHandler = generateReportController.submit()
     request.body = {
+      crmType: 'crm4',
       startDate: '2023-03-01',
       endDate: '2023-03-30',
       profileAcceptedTypes: '1,4,5,6',
@@ -48,16 +47,19 @@ xdescribe('downloadEvidenceController', () => {
     await requestHandler(request, response, next)
 
     expect(response.setHeader).toHaveBeenCalledWith('Content-Disposition', 'attachment; filename=crm4Report.csv')
-    expect(response.send).toHaveBeenCalledWith(downloadData)
+    expect(response.send).toHaveBeenCalledWith(crmReportResponse.text)
 
     expect(mockCrmReportApiService.getCrmReport).toHaveBeenCalledWith('2023-03-01', '2023-03-30', '1,4,5,6')
   })
 })
 
-const successResponse = (): CrmReportResponse => {
+const getCrmReportResponse = (): CrmReportResponse => {
   return {
     text:
-      ' `Client UFN,Usn,Provider Account,Firm Name,Client Name,Rep Order Number,Maat ID,Prison Law,Date Received,Decision Date,Decision,Expenditure Type,Expert Name,Quantity,Rate,Unit,Total Cost,Additional Expenditure,Total Authority,Total Granted,Granting Caseworker\n' +
-      '        031022/777,5001613,0D182J,ABELS,Joe modo,78543657,,No,2023-03-16,2023-03-16,Grant,a Psychiatrist,tyjtjtjt,4.0,50.0,Hour(s),200.0,0.0,200.0,200.0,Sym-G`',
+      'Client UFN,Usn,Provider Account,Firm Name,Client Name,Rep Order Number,Maat ID,Prison Law,Date Received,' +
+      'Decision Date,Decision,Expenditure Type,Expert Name,Quantity,Rate,Unit,Total Cost,Additional Expenditure,' +
+      'Total Authority,Total Granted,Granting Caseworker\n' +
+      '031022/777,5001613,0D182J,ABELS,Joe modo,78543657,,No,2023-03-16,2023-03-16,Grant,a Psychiatrist,' +
+      'tyjtjtjt,4.0,50.0,Hour(s),200.0,0.0,200.0,200.0,Sym-G`',
   }
 }
