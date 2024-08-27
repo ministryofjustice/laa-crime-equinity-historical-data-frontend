242c783994d26d0ef70e7534d5d21dca5e9fbd4f
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-08-27 17:03:44 +0100

    Added test case

--- server/services/generateReportService.test.ts
+++ server/services/generateReportService.test.ts
@@ -1,6 +1,7 @@
 import superagent from 'superagent'
 import GenerateReportService from './generateReportService'
 import CrmReportApiClient from '../data/api/crmReportApiClient'
+import { SanitisedError } from '../sanitisedError'
 
 jest.mock('../data/api/crmReportApiClient')
 
@@ -65,6 +66,41 @@ describe('Generate Report Service', () => {
       createdToDate: '2024-31-01',
       lastSubmittedFromDate: '',
       lastSubmittedToDate: '',
+      profileAcceptedTypes: '1,4,5,6',
+    })
+  })
+
+  it('should return error', async () => {
+    const error: SanitisedError = {
+      name: 'some error',
+      message: 'some message',
+      stack: 'some stack',
+      status: 404,
+      text: 'error',
+    }
+    mockCrmReportApiClient.getCrmReport.mockRejectedValue(error)
+
+    const generateReportService = new GenerateReportService(mockCrmReportApiClient)
+
+    const result = await generateReportService.getCrmReport({
+      crmType: 'crm4',
+      decisionFromDate: '2024-01-01',
+      decisionToDate: '2024-31-01',
+      profileAcceptedTypes: '1,4,5,6',
+    })
+
+    expect(result).toEqual({
+      error: {
+        message: 'some message',
+        status: 404,
+      },
+      text: null,
+    })
+
+    expect(mockCrmReportApiClient.getCrmReport).toHaveBeenCalledWith({
+      crmType: 'crm4',
+      decisionFromDate: '2024-01-01',
+      decisionToDate: '2024-31-01',
       profileAcceptedTypes: '1,4,5,6',
     })
   })
