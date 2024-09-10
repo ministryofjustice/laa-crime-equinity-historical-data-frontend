e8aaec3693b36af9ffc5b29452576c76af3b8294
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-09-10 15:41:31 +0100

    created crm14OfficeUseOnly.njk amended crm14DisplayConfig.json amended customDisplay.njk added mock data to crm14Controller.ts

--- server/controllers/crm/crm14Controller.ts
+++ server/controllers/crm/crm14Controller.ts
@@ -15,7 +15,54 @@ export default class Crm14Controller {
     return async (req: Request, res: Response): Promise<void> => {
       const usn = Number(req.params.usn)
       const { sectionId } = req.params
-      const crm14Response = await this.crm14Service.getCrm(usn, getProfileAcceptedTypes(res))
+      // const crm14Response = await this.crm14Service.getCrm(usn, getProfileAcceptedTypes(res))
+      const crm14Response = {
+        formDetails: {
+          officeUseOnly: {
+            benefitCheck: {
+              dwpOverallCheck: 'NOT_NEEDED',
+              dwpResultReferTo: 'Local Authority',
+              dwpCheckSurname: 'Smith',
+              dwpCheckNino: 'AB123456C',
+              dwpCheckBenefitCheckToRepeat: 'No',
+              dwpCheckPartnerSurname: 'Doe',
+              dwpCheckPartnerNino: 'CD654321E',
+            },
+            messageHistory: [
+              {
+                senderDisplay: 'Quasi Modo (Provider)',
+                message: 'Test message content',
+                senderUniqueName: 'ABELSABLES',
+                date: '18/Aug/2022 19:52',
+              },
+            ],
+            returnProvider: {
+              returnReason: 'Insufficient Evidence',
+              returnReasonDetails: 'The provider failed to provide sufficient supporting documents.',
+            },
+            fundingDecisions: [
+              {
+                maatNumber: 'MAAT123456',
+                caseNumber: 'CASE78910',
+                justiceTest: 'Passed',
+                meansTestResultType: 'Pass',
+                officialSignName: 'John Doe',
+                appropriateOfficerName: 'Jane Smith',
+                justiceTestReasons: 'Met all criteria',
+                overallResultMagsorcfs: 'Pass',
+                meansTestResultAppealtocc: 'Fail',
+                overallResultNonMeans: 'Pass',
+                meansTestResultMagsorcfs: 'Pass',
+                meansTestResultCc: 'Pass',
+                overallResultAppealtocc: 'Fail',
+                overallResultType: 'Pass',
+                overallResultCc: 'Pass',
+              },
+            ],
+          },
+        },
+      }
+
       const navigation = this.crmDisplayService.getNavigation('crm14', usn, sectionId, crm14Response)
       const sections = this.crmDisplayService.getSections('crm14', sectionId, crm14Response)
 
