aaef34e6a4d688cd8e186931f9fc61f259362162
Tosin <tosin.yusuf@digital.justice.gov.uk>
2024-09-11 16:27:38 +0100

    EMP-397 Added unit test

--- server/utils/crmFieldTransformer.test.ts
+++ server/utils/crmFieldTransformer.test.ts
@@ -5,7 +5,7 @@ describe('CRM Field Transformer', () => {
   it.each([
     ['courtType', 'M', 'Magistrate court'],
     ['every', '52', '1 week'],
-    ['levelOfWork', 'Advice', 'Advice & Assistance'],
+    ['yesNo', 1, 'Yes'],
     ['every', '999', '999'], // not transformed for unknown value
     ['blah', 'abc', 'abc'], // not transformed for unknown transform type
   ])('should transform "%s" value - %s', (transformType: TransformType, value: string, expected: string) => {
