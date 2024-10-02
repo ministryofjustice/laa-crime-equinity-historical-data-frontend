43d1428c97a1584f84a8313857fb20eb2386fd15
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-10-02 17:28:25 +0100

    created crmFurtherInformation.njk amended crm7DisplayConfig.json amended crm5DisplayConfig.json amended crm4DisplayConfig.json amended customDisplay.njk amended crmDisplayService.test.ts added mock api data to crm7Controller.ts

--- server/controllers/crm/crm7Controller.ts
+++ server/controllers/crm/crm7Controller.ts
@@ -15,7 +15,42 @@ export default class Crm7Controller {
     return async (req: Request, res: Response): Promise<void> => {
       const usn = Number(req.params.usn)
       const { sectionId } = req.params
-      const crm7Response = await this.crm7Service.getCrm(usn, getProfileAcceptedTypes(res))
+      // const crm7Response = await this.crm7Service.getCrm(usn, getProfileAcceptedTypes(res))
+      const crm7Response = {
+        formDetails: {
+          FurtherInformation: [
+            {
+              name: 'Postroom Notification CRM7',
+              description: 'Received By Post At Nottingham',
+              originalFileName: 'N/A',
+              dateReceived: '2024-07-10T12:09:14.000+00:00',
+              attachedPerson: 'Dennis, Ms. Nicola',
+              attachedPersonId: 'DENN-N',
+              downloadFile: '',
+            },
+            {
+              name: 'Attachment2',
+              description: 'Experts Quotation for LAA',
+              originalFileName: 'Ihtesham Hassan Expert Estimate= Dr JKH  .pdf',
+              dateReceived: '2017-01-04T13:30:03.000+00:00',
+              attachedPerson: 'Ghulam Sohail',
+              attachedPersonId: 'SOHAILGHULAM',
+              downloadFile: 'Retrieve...',
+              key: '1482802.att',
+            },
+            {
+              name: 'Attachment3',
+              description:
+                'Please note that the address at which our Expert is based is HMP Birmingham.\n\nThe full postal address is \n\nWinson Green Rd\nBirmingham\nB18 4AS',
+              originalFileName: 'Other information',
+              dateReceived: '2017-01-04T13:30:03.000+00:00',
+              attachedPerson: 'Ghulam Sohail',
+              attachedPersonId: 'SOHAILGHULAM',
+              downloadFile: '',
+            },
+          ],
+        },
+      }
       const navigation = this.crmDisplayService.getNavigation('crm7', usn, sectionId, crm7Response)
       const sections = this.crmDisplayService.getSections('crm7', sectionId, crm7Response)
 
