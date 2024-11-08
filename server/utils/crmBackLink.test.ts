4d13b2bdae4e83e4f1944a91ae988b989eae9e3a
Barry Nesbeth <barry.nesbeth@digital.justice.gov.uk>
2024-11-08 11:01:15 +0000

    amended crmBackLink.test.ts

--- server/utils/crmBackLink.test.ts
+++ server/utils/crmBackLink.test.ts
@@ -3,24 +3,21 @@ import manageBackLink from './crmBackLink'
 describe('manageBackLink', () => {
   it('should return search-eform link when current URL is a CRM page', () => {
     const currentUrl = '/crm5/1234567/general-information'
-    const lastVisitedSection = ''
-    const backUrl = manageBackLink(currentUrl, lastVisitedSection)
+    const backUrl = manageBackLink(currentUrl)
 
     expect(backUrl).toBe('/search-eform')
   })
 
   it('should return main landing page when current URL is the generate report page', () => {
     const currentUrl = '/generate-report'
-    const lastVisitedSection = ''
-    const backUrl = manageBackLink(currentUrl, lastVisitedSection)
+    const backUrl = manageBackLink(currentUrl)
 
     expect(backUrl).toBe('/')
   })
 
   it('should return main landing page by default for any other page', () => {
     const currentUrl = '/some-other-page'
-    const lastVisitedSection = ''
-    const backUrl = manageBackLink(currentUrl, lastVisitedSection)
+    const backUrl = manageBackLink(currentUrl)
 
     expect(backUrl).toBe('/')
   })
